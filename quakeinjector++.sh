6ef0f48ed076415fff727074e0de1a3fd3862dac
doopm8 <mailer@picklesrevil.com>
2015-08-04 14:22:56 -0400

    A non-working quake script. Needs the game files

---
+++ quakeinjector++.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get install quakespasm
+mkdir ~/documents/quakefrompicklesrevil/
+wget http://picklesrevil.com/Id1.zip -P ~/.quakespasm/id1
+sudo apt-get install zip
+cd ~/.quakespasm/id1
+unzip Id1.zip
